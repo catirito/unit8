97fc7db6e35ecadf23d4c41021bdde9ca402ee10
catirito <catirito30@gmail.com>
2015-05-08 00:09:16 +0200

    Revert "profesor acutalizado"
    This reverts commit 4c5bcae4c7e50828042d9896a6d83c8b62ccb888.

--- estudios/src/estudios/Profesor.java
+++ estudios/src/estudios/Profesor.java
@@ -9,22 +9,12 @@ package estudios;
  */
 public class Profesor {
 
-	int salario;
-	
 	public Profesor(){
 		
 	}
 	
 	public boolean esProfesor(){
 		return false;
-	}
-
-	public int getSalario() {
-		return salario;
-	}
-
-	public void setSalario(int salario) {
-		this.salario = salario;
 	}
 	
 
